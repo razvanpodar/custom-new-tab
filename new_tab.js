1b3d7dcaf6cf871a4f2abf70a50c49688098a46e
razvanpodar <razvan_podar@yahoo.com>
2021-11-22 11:28:29 +0200

    Finish general settings page from popup and fix chrome storage issues
    Changed the code that accesed the chrome storage because it wasn't
    getting the keys correctly. The general settings page now works
    properly.

---
+++ new_tab.js
@@ -0,0 +1,21 @@
+// console.log(chrome.storage.local.get['cnt-tab-name']);
+// document.title = chrome.storage.local.get['cnt-tab-name'];
+
+// For compatibility with other browsers like Firefox or Edge
+// chrome.storage needs to be changed to browser.storage
+// TODO: create function that deals with this and abstracts 
+// the get function from the storage api
+
+init();
+
+function init()
+{
+    chrome.storage.sync.get(["cnt_tab_name", "cnt_bg_color"], function(result) {
+        console.log('Tab name currently is ' + result.cnt_tab_name);
+        document.title = result.cnt_tab_name;
+        console.log('Background color currently is ' + result.cnt_bg_color);
+        document.body.style.backgroundColor = result.cnt_bg_color;
+    });
+}
+
+
--- popup.js
+++ popup.js
@@ -4,9 +4,9 @@ var htmlSources = ["popup/general.html", "popup/clock.html", "popup/days.html",
     "popup/weather.html", "popup/productivity.html"];
 var content = document.getElementById("content");
 
-setup();
+init();
 
-function setup()
+function init()
 {
     menuButtons = $("#menu").children();
 
@@ -15,18 +15,13 @@ function setup()
         menuButtons[i].onclick = function() {onClick(i)};
     }
     menuButtons[0].className = "menu-button-highlight";
-    $("#content").load(htmlSources[0]);
-}
-
-function resetButtonsColor()
-{
-    for (let i = 0; i < menuButtons.length; i++)
-    {
-        if (i != activeTab)
+    $("#content").load(htmlSources[0], function() {
+        if (document.getElementById("btn-save"))
         {
-            menuButtons[i].style.backgroundColor = null;
+            document.getElementById("btn-save").onclick = function() {onSave()};
+            document.getElementById("btn-reset").onclick = function() {onReset()};
         }
-    }
+    });
 }
 
 function onClick(tab)
@@ -35,10 +30,59 @@ function onClick(tab)
     if (activeTab != tab)
     {
         menuButtons[activeTab].className = "menu-button";
-        activeTab = tab;
-        $("#content").empty();
-        resetButtonsColor();
         menuButtons[tab].className = "menu-button-highlight";
-        $("#content").load(htmlSources[tab]);
+        $("#content").empty();
+        $("#content").load(htmlSources[tab], function() {
+            if (document.getElementById("btn-save"))
+            {
+                document.getElementById("btn-save").onclick = function() {onSave()};
+                document.getElementById("btn-reset").onclick = function() {onReset()};
+            }
+        });
+        activeTab = tab;
     }
+}
+
+function onSave()
+{
+    // Save input data to local storage
+    switch(activeTab)
+    {
+        case 0:
+            var tabName = $("#tab-name").val();
+            var bgColor = $("#bg-color").val();
+            if (tabName !== "")
+            {
+                // window.localStorage.setItem('cnt-title', tabName);
+                console.log(tabName);
+                // chrome.storage.local.set({'cnt-tab-name': tabName});
+                chrome.storage.sync.set({"cnt_tab_name": tabName, 
+                    "cnt_bg_color": bgColor}, function()
+                {
+                    console.log("Tab name is set to " + tabName);
+                    console.log("Background color is set to " + bgColor);
+                    $("#tab-name").val("");
+                });
+            }
+            break;
+    }
+    // Reload custom new tab page
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+    });
+}
+
+function onReset()
+{
+    switch(activeTab)
+    {
+        case 0:
+
+
+            break;
+    }
+    // Reset all values by deleting the keys from local storage
+    console.log("Reset values!");
+    // window.localStorage.removeItem('cnt-title');
+    // window.localStorage.remoteItem('cnt-bg-color');
 }
